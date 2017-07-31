f9fdecdd2124e6eec3cd6b97322fa9ac1774507b
LeyderMendieta <lmendieta6@misena.edu.co>
2017-07-31 15:16:18 -0500

    1.2.5
    sesion actualizada

--- claseLogin.php
+++ claseLogin.php
@@ -5,20 +5,15 @@ class Login
 	public $email;
 	public $password;
 	
-	public function __construct($Email,$Password,$Fk_user)
-	{
-		$this->email = $Email;
-		$this->password = $Password;
-		$this->fk_user = $Fk_user;
-	}
-	public function __construct2($Email,$Password)
+	public function __construct($Email,$Password)
 	{
 		$this->email = $Email;
 		$this->password = $Password;
 	}
 	
-	public function registrar()
+	public function registrar($Fk_user)
 	{
+		$this->fk_user = $Fk_user;
 		$sql5="INSERT INTO login (fk_user,email,password,confirmMail)
 		VALUES ('$this->fk_user', '$this->email', '".md5($this->password)."','0')";
 		if ($db->query($sql5) === TRUE)
@@ -34,6 +29,7 @@ class Login
 	
 	public function iniciarSesion()
 	{
+		session_start();
 		include('database.php');
 		$sql="SELECT * FROM login WHERE email='$this->email'";
 		if(!$result = $db->query($sql))
@@ -64,21 +60,19 @@ class Login
 					$nombre=stripslashes($row1["nombre"]);
 					$apellido=stripslashes($row1["apellido"]);
 					$numeroDocumento=stripslashes($row1["numeroDocumento"]);
-					$perfil=stripslashes($row1["perfil"]);
 					$entidad=stripslashes($row1["entidad"]);
 					
 					
 					$_SESSION["nombre"]=$nombre;
 					$_SESSION["apellido"]=$apellido;
 					$_SESSION["numeroDocumento"]=$numeroDocumento;
-					$_SESSION["perfil"]=$perfil;
 					$_SESSION["entidad"]=$entidad;
 					
 					
 					if($rolUsuario=="Medico")
 					{
 						$_SESSION["status"]="1";
-						header("Location: cuentaMedico.php");
+						//header("Location: cuentaMedico.php");
 					}
 					else
 					{
@@ -101,10 +95,6 @@ class Login
 			//header("Location: index.php");
 		}
 		else;
-		if(isset($usuario))
-		{
-			$this->idUsuario=$usuario;
-		}		
 	}
 	public function cerrarSesion()
 	{
--- clasePaciente.php
+++ clasePaciente.php
@@ -6,29 +6,7 @@ class Paciente extends Usuario
 	{
 		
 	}
-	public function registrar($nombre,$apellido,$fecha,$tipo,$documento,$rol)
-	{
-		include('database.php');
-		$sql="INSERT INTO usuario (nombre,apellido,tipoDocumento,numeroDocumento,rolUsuario,nacimiento,entidad,perfil,telefono)
-		VALUES ('$nombre', '$apellido', '$tipo','$documento','$rol','$fecha','0','profile/predeterminado.jpg','Ninguno')";
-		if ($db->query($sql) === TRUE)
-		{
-			$sql4="SELECT * FROM usuario WHERE tipoDocumento='$tipo' AND numeroDocumento='$documento'";
-			if(!$result4 = $db->query($sql4))
-			{
-				die('error al ejecutar la sentencia '. $db->error.']');
-			}
-			
-			while($row4 = $result4->fetch_assoc())
-			{
-				$idUsuario=stripslashes($row4["id_usuario"]);
-			}
-		}
-		else
-		{
-			echo "<h1>Error al registrar nuevo Usuario. Fail Query</h1>";
-		}
-	}
+	
 }
 $objetoPaciente = new Paciente();
 ?>
