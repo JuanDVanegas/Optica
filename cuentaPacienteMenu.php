607121026c0b920ffe4cdeb0f8e5af8c85f914a7
Juan David Vanegas Rodriguez <jdvanegas087@misena.edu.co>
2017-08-24 12:32:28 -0500

    1.3.05
    Iconos de menu actualizados

--- cuentaPacienteMenu.php
+++ cuentaPacienteMenu.php
@@ -1,5 +1,5 @@
 <div class="vertical-menu">
- <a href="cuentaPacienteInicio.php" target="_parent">Inicio</a>
- <a href="cuentaPacientePerfil.php" target="_parent">Perfil</a>
- <a href="cuentaPacienteHistorial.php" target="_parent">Historial</a>
+ <a href="cuentaPacienteInicio.php" target="_parent">Inicio<span class="glyphicon glyphicon-home pull-right" aria-hidden="true" /></a>
+ <a href="cuentaPacientePerfil.php" target="_parent">Perfil<span class="glyphicon glyphicon-user pull-right" aria-hidden="true" /></a>
+ <a href="cuentaPacienteHistorial.php" target="_parent">Historial<span class="glyphicon glyphicon-list-alt pull-right" aria-hidden="true" /></a>
 </div>
