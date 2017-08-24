f59367f9c715a1a995c22a4d25bf2a8fddd293cf
Juan David Vanegas Rodriguez <jdvanegas087@misena.edu.co>
2017-08-24 13:44:49 -0500

    1.3.08
    iconos actualizados ._.

--- cuentaAdminMenu.php
+++ cuentaAdminMenu.php
@@ -1,7 +1,7 @@
 <div class="vertical-menu">
  <a href="cuentaAdminInicio.php" target="_parent">Inicio<span class="glyphicon glyphicon-home pull-right" aria-hidden="true" /></a>
  <a href="cuentaAdminPerfil.php" target="_parent">Perfil<span class="glyphicon glyphicon-user pull-right" aria-hidden="true" /></a>
- <a href="cuentaAdminEntidades.php" target="_parent">Entidades<span class="glyphicon glyphicon-wrench pull-right" aria-hidden="true" /></a>
- <a href="cuentaAdminNuevoUsuario.php" target="_parent">Nuevo Usuario<span class="glyphicon glyphicon-wrench pull-right" aria-hidden="true" /></a>
- <a href="cuentaAdminEliminarUsuario.php" target="_parent">Eliminar Usuario<span class="glyphicon glyphicon-wrench pull-right" aria-hidden="true" /></a>
+ <a href="cuentaAdminEntidades.php" target="_parent">Entidades<span class="glyphicon glyphicon-edit pull-right" aria-hidden="true" /></a>
+ <a href="cuentaAdminNuevoUsuario.php" target="_parent">Nuevo Usuario<span class="glyphicon glyphicon-edit pull-right" aria-hidden="true" /></a>
+ <a href="cuentaAdminEliminarUsuario.php" target="_parent">Eliminar Usuario<span class="glyphicon glyphicon-edit pull-right" aria-hidden="true" /></a>
 </div>
